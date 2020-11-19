b6aa1247bf1f631250319c6cefd025d4f2f5a8c0
Atharva Mishra <atharvamishra956@gmail.com>
2020-11-19 19:19:09 +0530

    added database libraries with init file

---
+++ database-libraries/firbase-init.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDUYVM62eOzhNRpyyCNCREuP2RAN1zBAn8",
+    authDomain: "playersavedata.firebaseapp.com",
+    databaseURL: "https://playersavedata.firebaseio.com",
+    projectId: "playersavedata",
+    storageBucket: "playersavedata.appspot.com",
+    messagingSenderId: "181626447027",
+    appId: "1:181626447027:web:780cc53b9da65503f67cc6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
